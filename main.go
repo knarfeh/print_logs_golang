c2ef1e7f3e5532cd20d20293acbfde63ceb9100b
knarfeh <junh@alauda.io>
2016-12-26 20:09:34 +0800

    Fix Dockerfile, use go runtime

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func loop(done chan bool) {
+	for i := 0; i < 25000; i++ {
+		fmt.Printf("balabalabalabalabalabalabalabalabalabalabalabalabala\n")
+	}
+	done <- true
+}
 
 func main() {
-	fmt.Print("test")
+	t1 := time.Now()
+	runtime.GOMAXPROCS(4)
+	done := make(chan bool)
+	go loop(done)
+	go loop(done)
+	go loop(done)
+	go loop(done)
+
+	<-done
+	<-done
+	<-done
+	<-done
+	t2 := time.Now()
+	fmt.Println("time span: ", t2.Sub(t1))
 }
